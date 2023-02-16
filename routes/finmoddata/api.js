907a33f7930d2dbdd1beaea56fb77313f30704ea
Dylan <alloydylan@icloud.com>
2023-02-16 14:07:44 -0500

    🚧 feat(wip): added API ends for modeling equities

--- routes/finmoddata/api.js
+++ routes/finmoddata/api.js
@@ -2,17 +2,32 @@ const https = require("axios");
 const config = require('../../config')
 const tdaDataHost = config.apiHost.tdaData
 
-class Historical {
+class FinMod {
     /**
      * It returns the options data for a given query.
      * @param query - The query object that you want to send to the TDA API.
      * @returns An array of objects.
      */
-    static stock(query) {
-        return https.post(`${tdaDataHost}/finmod/historical`, {"query":query}).then((data)=>{return data.data}).catch(e=>{return e});
+    static stockPrices(query) {
+        return https.post(`${tdaDataHost}/finmod/historicalPrices`, {"query":query}).then((data)=>{return data.data}).catch(e=>{return e});
+    }
+    static secFiling(query) {
+        return https.post(`${tdaDataHost}/finmod/secFiling`, {"query":query}).then((data)=>{return data.data}).catch(e=>{return e});
+    }
+    static financialGrowth(query) {
+        return https.post(`${tdaDataHost}/finmod/financialGrowth`, {"query":query}).then((data)=>{return data.data}).catch(e=>{return e});
+    }
+    static dcf(query) {
+        return https.post(`${tdaDataHost}/finmod/dcf`, {"query":query}).then((data)=>{return data.data}).catch(e=>{return e});
+    }
+    static erTranscript(query) {
+        return https.post(`${tdaDataHost}/finmod/erTranscript`, {"query":query}).then((data)=>{return data.data}).catch(e=>{return e});
+    }
+    static senateDisclosure(query) {
+        return https.post(`${tdaDataHost}/finmod/senateDisclosure`, {"query":query}).then((data)=>{return data.data}).catch(e=>{return e});
     }
 }
 
 module.exports = {
-    Historical
+    FinMod
 }
--- routes/finmoddata/index.js
+++ routes/finmoddata/index.js
@@ -8,8 +8,38 @@ const api = require('./api')
 /* A POST request to the /historical route. It is using the stock function from the api.Historical
 object. It is passing the req.body as the parameter to the stock function. It is then returning the
 data as a json object. */
-finModRouter.post("/historical", async function (req, res) {
-    api.Historical.stock(req.body).then((data)=>{
+finModRouter.post("/historicalPrices", async function (req, res) {
+    api.FinMod.historicalPrices(req.body).then((data)=>{
+        res.json(new SuccessModel(data));
+    })
+});
+
+finModRouter.post("/secFiling", async function (req, res) {
+    api.FinMod.secFiling(req.body).then((data)=>{
+        res.json(new SuccessModel(data));
+    })
+});
+
+finModRouter.post("/financialGrowth", async function (req, res) {
+    api.FinMod.financialGrowth(req.body).then((data)=>{
+        res.json(new SuccessModel(data));
+    })
+});
+
+finModRouter.post("/dcf", async function (req, res) {
+    api.FinMod.dcf(req.body).then((data)=>{
+        res.json(new SuccessModel(data));
+    })
+});
+
+finModRouter.post("/erTranscript", async function (req, res) {
+    api.FinMod.erTranscript(req.body).then((data)=>{
+        res.json(new SuccessModel(data));
+    })
+});
+
+finModRouter.post("/senateDisclosure", async function (req, res) {
+    api.FinMod.senateDisclosure(req.body).then((data)=>{
         res.json(new SuccessModel(data));
     })
 });
